438234f3285a1814e82fc455e89e2bd98baf0167
johnnyleek <leek1j@cmich.edu>
2021-07-14 21:46:08 -0400

    allow you to actually select the grid again

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,7 +4,7 @@ import GridElement from './GridElement';
 import '../styles/grid.scss';
 
 export default React.forwardRef(({rows, cols}, ref) => {
-    const [grid, setGrid] = useState({});
+    const [grid, setGrid] = useState({ });
 
     useEffect(() => console.log(grid), [grid]);
 
@@ -23,13 +23,13 @@ export default React.forwardRef(({rows, cols}, ref) => {
 
     useEffect(() => console.log(rows, cols), [rows, cols]);
 
-    const updateGrid = (index) => { setGrid({ ...grid, [index]: !grid[index] }); } 
+    const updateGrid = (row, col) => { setGrid({ ...grid, [row]: { ...grid[row], [col]: !grid[row][col] } }); } 
 
     return (
       <div id="grid" ref={ref}>
           {[...Array(rows)].map( (row, i) => {
               return [...Array(cols)].map( (cols, j) => {
-                return <GridElement row={i} col={j} selected={grid[i][j]} onClick={updateGrid} key={`${i}-${j}`} />;
+                return <GridElement row={i} col={j} selected={grid[0] ? grid[i][j] : false} onClick={updateGrid} key={`${i}-${j}`} />;
               })
           })}
       </div>
--- src/components/GridElement.jsx
+++ src/components/GridElement.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export default function GridElement({row, col, selected, onClick}) {
 
     return (
-        <div className={`grid-item ${selected ? 'selected' : ''}`} onClick={() => onClick(row, col)}>{row}</div>
+        <div className={`grid-item${selected ? ' selected' : ''}`} onClick={() => onClick(row, col)}>{row}</div>
     );
 }
